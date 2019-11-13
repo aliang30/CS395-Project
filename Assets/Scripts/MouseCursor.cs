d73074fbf71c00d2502412ef3e26f014d6cd5fee
aliang30 <aliang30@binghamton>
2019-11-13 18:31:04 -0500

    Added new mouse cursor/cursor animation

--- Assets/Scripts/MouseCursor.cs
+++ Assets/Scripts/MouseCursor.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class MouseCursor : MonoBehaviour
 {
+	private SpriteRenderer rend;
+	public Sprite handCursor;
+	public Sprite normalCursor;
+	
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
-        Screen.lockCursor = true;
-        Screen.lockCursor = false;
+		rend = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //transform.position = cursorPos;
-        Vector2 sensitivity = new Vector2(.005f, .005f);
-        //Vector2 mouseMovement = new Vector2(Input.GetAxisRaw("Mouse X") * sensitivity.x, Input.GetAxisRaw("Mouse Y") * sensitivity.y);
-        Vector2 mouseMovement = new Vector2(Input.GetAxisRaw("Mouse X") * sensitivity.x, Input.GetAxisRaw("Mouse Y")*sensitivity.y);
-        //console.log(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        //Debug.Log(mouseMovement);
-        transform.Translate(mouseMovement.x,mouseMovement.y, 0, Space.Self);
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+		
+		if (Input.GetMouseButtonDown(0)){
+			rend.sprite = handCursor;
+			Vector2 sensitivity = new Vector2(.005f, .005f);
+			Vector2 mouseMovement = new Vector2(Input.GetAxisRaw("Mouse X") * sensitivity.x, Input.GetAxisRaw("Mouse Y") * sensitivity.y);
+
+			//console.log(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+			//Debug.Log(mouseMovement);
+			transform.Translate(mouseMovement.x,mouseMovement.y, 0, Space.Self);
+		} 
+		else if (Input.GetMouseButtonUp(0)){
+			rend.sprite = normalCursor;
+		}
+      
     }
 }
--- Assets/lockCursor.cs
+++ Assets/lockCursor.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class lockCursor : MonoBehaviour
 {
-	void Start() {
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-	}
-	
     // Update is called once per frame
     void Update()
     {
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-		
+		if (Input.GetKeyDown("1")) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
 		if (Input.GetKeyDown("2")) {
 			Cursor.lockState = CursorLockMode.None;
 			Cursor.visible = true;
